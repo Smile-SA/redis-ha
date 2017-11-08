ce57f92acf36c161fa050b1a3e4f087e0990e62b
Patrice FERLET <metal3d@gmail.com>
2017-11-08 10:00:15 +0100

    Changing startup script

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
 # Openshift Mode
-
 if [ $MODE == "statefulset" ] && [ $SENTINEL != "true" ] ; then
 
-
-    # first node is a master
+    # first statefulset pod is a master
     h=$(hostname)
     if [ $(echo "$h" | grep '\-0') == "-0" ]; then
+        echo "Master mode"
         exec "$@"
     fi
 
 
-    # else, slave (sentinel will start, so wait for it)
+    # else it's a slave (sentinel will start, so wait for it)
     CONN=""
     SENTINEL_HOST="127.0.0.1"
     until [ "$CONN" == "ok" ]; do
+        echo "wait for sentinel..."
         nc --send-only $SENTINEL_HOST 26379 < /dev/null && CONN="ok" || sleep 1
     done
 
     CONN=""
     until [ "$CONN" == "ok" ]; do
         master=$(redis-cli -h $SENTINEL_HOST -p 26379 SENTINEL get-master-addr-by-name mymaster | head -n1)
+        echo "master is: ${master}, checking..."
         nc --send-only $master 6379 < /dev/null && CONN="ok" || sleep 1
     done
 
+    # let's change the state
     sed -i 's/^slaveof master 6379/slaveof '${master}' 6379/' /etc/redis-slave.conf
     exec redis-server /etc/redis-slave.conf
 fi
@@ -32,10 +34,18 @@ fi
 # else, startup as expected
 
 if [ "$SENTINEL" == "true" ]; then
+    echo "Sentinel mode"
     if [ $MODE == "statefulset" ]; then
+        # we change xxx-1 to xxx-0 that is the master pod
         h=$(hostname -f | sed -r 's/-[0-9]+/-0/')
         sed -i 's/monitor mymaster master/monitor mymaster '$h'/' /etc/redis-sentinel.conf 
+        # check that master is up
+        CONN=""
+        until [ "$CONN" == "ok" ]; do
+            nc --send-only $h 26379 < /dev/null && CONN="ok" || sleep 1
+        done
     fi
+
     exec redis-sentinel /etc/redis-sentinel.conf
 fi
 
@@ -49,15 +59,18 @@ if [ "$SLAVE" == "true" ]; then
     # Because the initial master will be down, we need to get master address
     # via sentinel
 
+    echo "Slave mode"
     CONN=""
     [ "$SENTINEL_HOST" == "" ] && SENTINEL_HOST="127.0.0.1"
     until [ "$CONN" == "ok" ]; do
+        echo "Waiting sentinel ${SENTINEL_HOST}..."
         nc --send-only $SENTINEL_HOST 26379 < /dev/null && CONN="ok" || sleep 1
     done
 
     CONN=""
     until [ "$CONN" == "ok" ]; do
         master=$(redis-cli -h $SENTINEL_HOST -p 26379 SENTINEL get-master-addr-by-name mymaster | head -n1)
+        echo "master is: ${master}, checking..."
         nc --send-only $master 6379 < /dev/null && CONN="ok" || sleep 1
     done
 
