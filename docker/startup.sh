761fc9baae098ed6494d9022237d282548ba01d3
Patrice FERLET <metal3d@gmail.com>
2017-11-08 10:19:43 +0100

    fixed grep command

--- docker/startup.sh
+++ docker/startup.sh
@@ -5,9 +5,7 @@ if [ "$MODE" == "statefulset" ] && [ "$SENTINEL" != "true" ] ; then
 
     # first statefulset pod is a master
     h=$(hostname)
-    echo $h
-    echo "$h" | grep '\-0'
-    if [ $(echo "$h" | grep '\-0') == "-0" ]; then
+    if [ $(echo "$h" | grep -o '\-0$') == "-0" ]; then
         echo "Master mode"
         exec "$@"
     fi
